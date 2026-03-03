037825bfeb2898510b772c1376a2e7e9eb705ce7
minimarimo3 <minimarimo3@gmail.com>
2026-03-03 18:06:49 +0900

    設定画面強化

--- Yuukei/Assets/Scripts/SettingsUIManager.cs
+++ Yuukei/Assets/Scripts/SettingsUIManager.cs
@@ -1,6 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Kirurobo;
 
 [RequireComponent(typeof(UIDocument))]
 public class SettingsUIManager : MonoBehaviour
@@ -10,28 +10,55 @@ public class SettingsUIManager : MonoBehaviour
     private VisualElement _settingsPanel;
     private VisualElement _dragHandle;
     private Button _closeButton;
-    private ScrollView _contentArea;
-
-    // TODO: ご使用のUniWindowControllerのコンポーネントを参照してください
-    [SerializeField] private UniWindowController _windowController;
+    
+    // コンテンツエリア
+    private Label _pageTitle;
+    private ScrollView _activePageContent;
+    private Label _placeholderText;
 
     private PanelDragManipulator _dragManipulator;
 
-    public VisualElement ContentArea => _contentArea;
+    // タブ名とページタイトルのマッピング
+    private readonly Dictionary<string, string> _tabTitleMap = new Dictionary<string, string>()
+    {
+        { "Tab_General", "一般設定" },
+        { "Tab_Character", "キャラクターの選択" },
+        { "Tab_Script", "台本の選択" },
+        { "Tab_Prop", "小物（オブジェクト）の選択" },
+        { "Tab_Theme", "ウィンドウ・テーマの選択" },
+        { "Tab_Mod", "MOD（プラグイン）の管理" },
+        { "Tab_Package", "パッケージの管理" },
+        { "Tab_Market", "マーケットプレイス" },
+        { "Tab_About", "アプリについて (About)" }
+    };
 
     private void Awake()
     {
         _uiDocument = GetComponent<UIDocument>();
-        _rootContainer = _uiDocument.rootVisualElement.Q<VisualElement>("RootContainer");
-        _settingsPanel = _uiDocument.rootVisualElement.Q<VisualElement>("SettingsPanel");
-        _dragHandle = _uiDocument.rootVisualElement.Q<VisualElement>("DragHandle");
-        _closeButton = _uiDocument.rootVisualElement.Q<Button>("CloseButton");
-        _contentArea = _uiDocument.rootVisualElement.Q<ScrollView>("ContentArea");
+        var root = _uiDocument.rootVisualElement;
+
+        _rootContainer = root.Q<VisualElement>("RootContainer");
+        _settingsPanel = root.Q<VisualElement>("SettingsPanel");
+        _dragHandle = root.Q<VisualElement>("DragHandle");
+        _closeButton = root.Q<Button>("CloseButton");
+        
+        _pageTitle = root.Q<Label>("PageTitle");
+        _activePageContent = root.Q<ScrollView>("ActivePageContent");
+        _placeholderText = root.Q<Label>("PlaceholderText");
 
         _closeButton.clicked += HideSettings;
-
-        // ドラッグ操作のバインド
         _dragManipulator = new PanelDragManipulator(_dragHandle, _settingsPanel);
+
+        // タブボタンのイベント登録
+        foreach (var kvp in _tabTitleMap)
+        {
+            var tabButton = root.Q<Button>(kvp.Key);
+            if (tabButton != null)
+            {
+                // クリックされたら SwitchPage メソッドを呼ぶ
+                tabButton.clicked += () => SwitchPage(kvp.Key, kvp.Value);
+            }
+        }
 
         HideSettings();
     }
@@ -39,10 +66,29 @@ public class SettingsUIManager : MonoBehaviour
     public void ShowSettings()
     {
         _rootContainer.style.display = DisplayStyle.Flex;
+        // 開いたときは常に「一般設定」を表示する
+        SwitchPage("Tab_General", _tabTitleMap["Tab_General"]);
     }
 
     public void HideSettings()
     {
         _rootContainer.style.display = DisplayStyle.None;
+    }
+
+    private void SwitchPage(string tabId, string pageTitle)
+    {
+        // タイトルを更新
+        _pageTitle.text = pageTitle;
+
+        // 一旦中身をクリアする（将来的に動的生成したUI要素を入れるため）
+        _activePageContent.Clear();
+
+        // TODO: ここで選択されたタブに応じた固有のUI（UXML）をロードしてアタッチする処理を書く
+        // 今回はプレースホルダーのテキストを書き換えて追加しておく
+        Label infoLabel = new Label($"現在のページ: {pageTitle}\n\n※このページの内容は現在準備中です。");
+        infoLabel.style.color = new StyleColor(new Color(0.8f, 0.8f, 0.8f));
+        infoLabel.style.marginTop = 10;
+        
+        _activePageContent.Add(infoLabel);
     }
 }
